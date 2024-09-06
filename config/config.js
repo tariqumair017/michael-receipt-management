8df6a10377956d514e4bb6baa8bc8e24c38220a1
Umair Tariq <86774981+tariqumair017@users.noreply.github.com>
2024-09-06 23:48:47 +0500

    full N final commit

---
+++ config/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    PORT: process.env.PORT || 8888,
+    DB_URL: process.env.DB_URL,
+    BUCKET: process.env.BUCKET,
+    REGION: process.env.REGION,
+    AWS_ACCESS_KEY: process.env.AWS_ACCESS_KEY,
+    AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY
+};
